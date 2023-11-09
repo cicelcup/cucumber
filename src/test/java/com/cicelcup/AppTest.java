e2d0ffe0da1b9dafa523de3f0d6d3dc75722a4c0
Jorge A Peroza M <cicelcup@hotmail.com>
2023-11-09 16:57:02 -0600

    Add Test annotation

--- src/test/java/com/cicelcup/AppTest.java
+++ src/test/java/com/cicelcup/AppTest.java
@@ -1,6 +1,7 @@
 package com.cicelcup;
 
 import org.testng.Assert;
+import org.testng.annotations.Test;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -11,19 +12,20 @@ public class AppTest
     public int result = 0;
     public int a = 0;
     public int b = 0;
-
+    @Test(priority = 0)
     @Given("With two numbers 2,3")
     public void with_two_numbers() {
         a = 2;
         b = 3;
     }
-
+    @Test(priority = 1)
     @When("add the numbers")
     public void add_the_numbers() {
         Calculator calculator = new Calculator();
         result = calculator.add(a, b);
     }
 
+    @Test(priority = 2)
     @Then("the result should be 5")
     public void the_result_should_be() {
         Assert.assertEquals(result, 5);
