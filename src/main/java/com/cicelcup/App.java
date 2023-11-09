669aec9c62eefa0af62f16f7ea2b0895728f1764
Jorge A Peroza M <cicelcup@hotmail.com>
2023-11-09 16:28:37 -0600

    Start project

---
+++ src/main/java/com/cicelcup/App.java
@@ -0,0 +1,9 @@
+package com.cicelcup;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/cicelcup/Calculator.java
@@ -0,0 +1,5 @@
+package com.cicelcup;
+
+public class Calculator {
+    
+}
