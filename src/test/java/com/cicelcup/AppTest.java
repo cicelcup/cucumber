435838a7ea69591b422adcf6d0193ac36ef74281
Jorge A Peroza M <cicelcup@hotmail.com>
2023-11-09 17:05:02 -0600

    Remove test from file

--- src/test/java/com/cicelcup/AppTest.java
+++ src/test/java/com/cicelcup/AppTest.java
@@ -1,7 +1,6 @@
 package com.cicelcup;
 
 import org.testng.Assert;
-import org.testng.annotations.Test;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -12,20 +11,18 @@ public class AppTest
     public int result = 0;
     public int a = 0;
     public int b = 0;
-    @Test(priority = 0)
+
     @Given("With two numbers 2,3")
     public void with_two_numbers() {
         a = 2;
         b = 3;
     }
-    @Test(priority = 1)
     @When("add the numbers")
     public void add_the_numbers() {
         Calculator calculator = new Calculator();
         result = calculator.add(a, b);
     }
 
-    @Test(priority = 2)
     @Then("the result should be 5")
     public void the_result_should_be() {
         Assert.assertEquals(result, 5);
