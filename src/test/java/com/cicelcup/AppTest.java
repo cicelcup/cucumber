1571865b652e338b390eb960954561ebfd9ab8c2
Jorge A Peroza M <cicelcup@hotmail.com>
2023-11-09 16:48:33 -0600

    Add test

--- src/test/java/com/cicelcup/AppTest.java
+++ src/test/java/com/cicelcup/AppTest.java
@@ -1,11 +1,31 @@
 package com.cicelcup;
 
+import org.testng.Assert;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest 
 {
+    public int result = 0;
+    public int a = 0;
+    public int b = 0;
 
+    @Given("With two numbers 2,3")
+    public void with_two_numbers() {
+        a = 2;
+        b = 3;
+    }
+
+    @When("add the numbers")
+    public void add_the_numbers() {
+        Calculator calculator = new Calculator();
+        result = calculator.add(a, b);
+    }
+
+    @Then("the result should be 5")
+    public void the_result_should_be() {
+        Assert.assertEquals(result, 5);
+    }
 }
